90fcab2149248425714dd21871de7e3a118b6a58
NiBaNo <157634942+NiBaNo@users.noreply.github.com>
2026-08-27 15:28:25 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,8 @@
 
+const SUPABASE_URL = "https://xkwzddjzypnskakskell.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_mRv3S9r8wbop2EyPR5Icyg_bTAHwYNz";
+
+export const supabase = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
